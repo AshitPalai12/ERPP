66769ecb072340d246973927e933442882cc89b0
Vipeen-1 <Vipeen.Kumar@Cubation.com>
2024-01-16 14:49:07 +0530

    user-list updated

--- src/app/Pages/container/user-detail/user-detail.component.ts
+++ src/app/Pages/container/user-detail/user-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-user-detail',
+  selector: 'user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
