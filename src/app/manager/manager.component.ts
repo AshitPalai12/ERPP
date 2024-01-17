c0aeb715498886d679c9be9fef1df4ce91367e6b
AshitPalai12 <ashit.palai@cubastion.com>
2024-01-17 14:54:02 +0530

    did work on manager component

--- src/app/manager/manager.component.ts
+++ src/app/manager/manager.component.ts
@@ -1,37 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-manager',
   templateUrl: './manager.component.html',
   styleUrls: ['./manager.component.css']
 })
-export class ManagerComponent {
-  userForm!:FormGroup
+export class ManagerComponent  implements OnInit{
+  managerForm!:FormGroup
 
-  constructor(private fb:FormBuilder){
+  constructor(private fb:FormBuilder ,private toastr:ToastrService,private service:ApiService){
     
 
   }
 
   ngOnInit(){
 
-    this.userForm = this.fb.group({
-      username:[null,[Validators.required]],
-      password:[null,[Validators.required]],
-      role:[null,[Validators.required]],
-      department:[null,[Validators.required]]
+    this.managerForm = this.fb.group({
+      id:this.fb.control('',Validators.required),
+      name:this.fb.control('',Validators.required),
+      password:this.fb.control('',Validators.required),
+      email:this.fb.control('',Validators.required),
+      department:this.fb.control('',Validators.required)
     })
 
   }
+  
 
-  onFormSubmit(){
-
+  loadManager(){
+   if (this.managerForm.valid){
+    this.service.postManager(this.managerForm.value).subscribe((res)=>{
+      this.toastr.success(("Manager has been successfully registered"))
+      this.managerForm.reset()
+    })
+   }
   }
   
   onEmptySubmit(){
-    if(this.userForm.invalid){
-      alert('Please enter the required fields!!')
+    if(this.managerForm.invalid){
+      this.toastr.error('Please enter the required fields!!')
     }
   }
 }
+``
