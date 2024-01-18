2dd7b89504f08b71cdea3ef866cbcb1451adc11d
Vipeen-1 <Vipeen.Kumar@Cubation.com>
2024-01-18 15:43:37 +0530

    user-detail view more

--- src/app/Pages/container/user-detail/user-detail.component.ts
+++ src/app/Pages/container/user-detail/user-detail.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../model/userInter';
+import { ApiService } from 'src/app/services/api.service';
+import { HttpParams } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css']
 })
-export class UserDetailComponent {
+export class UserDetailComponent implements OnInit {
+
+  selectedUser:any;
+
+  constructor(private _apiservice:ApiService,private activatedRoute:ActivatedRoute){}
+
+  ngOnInit(){
+    this.activatedRoute.params.subscribe((params)=>{
+      let id = params['id']
+      this._apiservice.getUserById(id).subscribe((data)=>{
+        console.log(data);
+        this.selectedUser= data;
+        console.log(this.selectedUser);
+      })
+      // console.log(params);
+    })
+  }
+
+  
+
+
+
 
 }
